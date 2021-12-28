9f373b7de1f36796b2157917e2937dd67cdf026d
komunre <koliziy5@gmail.com>
2021-12-29 01:57:31 +0700

    works, but kind of bad

---
+++ Source/PassiveResearch/PassiveResearch/PassiveProgress.cs
@@ -0,0 +1,43 @@
+﻿using Verse;
+
+namespace PassiveResearch
+{
+    public class PassiveProgress : MapComponent
+    {
+        public float PassiveResearchSpeed = 0.1f;
+        protected PassiveResearchWindow window = null;
+        public PassiveProgress(Map map) : base(map)
+        {
+        }
+
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+
+            if (!map.IsPlayerHome) return;
+
+            var manager = Find.ResearchManager;
+            if (manager.currentProj == null) return;
+
+            manager.ResearchPerformed(PassiveResearchSpeed, null);
+        }
+
+        public override void MapComponentOnGUI()
+        {
+            base.MapComponentOnGUI();
+
+            if (window == null)
+            {
+                window = new PassiveResearchWindow();
+                Find.WindowStack.Add(window);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref PassiveResearchSpeed, "passiveResearchSpeed");
+        }
+    }
+}
---
+++ Source/PassiveResearch/PassiveResearch/PassiveResearchWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using System.Globalization;
+
+namespace PassiveResearch
+{
+    public class PassiveResearchWindow : Window
+    {
+        private string speedValue;
+        public PassiveResearchWindow()
+        {
+            draggable = true;
+            closeOnCancel = false;
+            closeOnAccept = false;
+            preventCameraMotion = false;
+            focusWhenOpened = false;
+            speedValue = "0.1";
+        }
+        public override void PostOpen()
+        {
+            base.PostOpen();
+
+            windowRect = new Rect(0, 0, 160, 90);
+        }
+        public override void DoWindowContents(Rect inRect)
+        {
+            speedValue = Widgets.TextField(new Rect(0, 0, 40, 20), speedValue, 20);
+            if (Widgets.ButtonText(new Rect(0, 20, 80, 20), "Apply"))
+            {
+                var map = Find.CurrentMap;
+                try
+                {
+                    map.GetComponent<PassiveProgress>().PassiveResearchSpeed = float.Parse(speedValue, CultureInfo.InvariantCulture);
+                } catch
+                {
+                    
+                }
+            }
+        }
+    }
+}
